320bc8e9ad151c0c5a9dd722caf7d1bdd729e614
Zubenathi Mateza <122874188+Zubenathi-cp@users.noreply.github.com>
2023-07-31 12:40:50 +0200

    Add files via upload

---
+++ The HangMan 2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace The_HangMan_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnQuitGame = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblWordLength = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblIncorrectGuesses = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.BackgroundImage = global::The_HangMan_2.Properties.Resources.R2;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnQuitGame);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btnNewGame);
+            this.groupBox1.Controls.Add(this.lblWordLength);
+            this.groupBox1.Controls.Add(this.lblOutput);
+            this.groupBox1.Controls.Add(this.lblIncorrectGuesses);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblPlayerScore);
+            this.groupBox1.Location = new System.Drawing.Point(4, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1006, 426);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnQuitGame
+            // 
+            this.btnQuitGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitGame.Location = new System.Drawing.Point(844, 387);
+            this.btnQuitGame.Name = "btnQuitGame";
+            this.btnQuitGame.Size = new System.Drawing.Size(126, 33);
+            this.btnQuitGame.TabIndex = 2;
+            this.btnQuitGame.Text = "Quit Game";
+            this.btnQuitGame.UseVisualStyleBackColor = true;
+            this.btnQuitGame.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(731, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(12, 387);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(123, 33);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblWordLength
+            // 
+            this.lblWordLength.AutoSize = true;
+            this.lblWordLength.BackColor = System.Drawing.SystemColors.Control;
+            this.lblWordLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordLength.Location = new System.Drawing.Point(711, 162);
+            this.lblWordLength.Name = "lblWordLength";
+            this.lblWordLength.Size = new System.Drawing.Size(109, 20);
+            this.lblWordLength.TabIndex = 4;
+            this.lblWordLength.Text = "Word Length :";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.lblOutput.Location = new System.Drawing.Point(291, 52);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(193, 13);
+            this.lblOutput.TabIndex = 12;
+            this.lblOutput.Text = "Please press a letter from the keyboard!";
+            this.lblOutput.Click += new System.EventHandler(this.lblOutput_Click);
+            // 
+            // lblIncorrectGuesses
+            // 
+            this.lblIncorrectGuesses.AutoSize = true;
+            this.lblIncorrectGuesses.BackColor = System.Drawing.SystemColors.Control;
+            this.lblIncorrectGuesses.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncorrectGuesses.Location = new System.Drawing.Point(8, 271);
+            this.lblIncorrectGuesses.Name = "lblIncorrectGuesses";
+            this.lblIncorrectGuesses.Size = new System.Drawing.Size(148, 20);
+            this.lblIncorrectGuesses.TabIndex = 6;
+            this.lblIncorrectGuesses.Text = "Incorrect Guesses :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(254, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(274, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Welcome to the hangman !!";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.BackColor = System.Drawing.SystemColors.Control;
+            this.lblPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerScore.Location = new System.Drawing.Point(714, 231);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(106, 20);
+            this.lblPlayerScore.TabIndex = 5;
+            this.lblPlayerScore.Text = "Player Score :";
+            this.lblPlayerScore.Click += new System.EventHandler(this.lblPlayerScore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(575, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "HINT: Colours!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1022, 450);
+            this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "f";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnQuitGame;
+        private System.Windows.Forms.Label lblWordLength;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblIncorrectGuesses;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ The HangMan 2/Form1.cs
@@ -0,0 +1,229 @@
+﻿//Author : Zubenathi Mateza
+//File Name : The Hangman
+//Module Name : Basic c# programming
+//Project Number : 1
+//Date : 29 March 2023
+//Descriotion : This is a game called 'The Hangman', a player is asked to enter letters in order to correctly guess the given word!
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_HangMan_2
+{
+
+    public partial class Form1 : Form
+    {
+        //These are fields that will be needed in the code below
+        int score = 0;
+        string word = "";
+        List<Label> labels = new List<Label>();
+        int tries = 0;
+
+       
+
+        public void LabelsChar()
+        {
+             word = randoWord();
+
+
+            // Array for the characters
+            char[] chars = word.ToCharArray();
+            int spaceBetween = 330 / chars.Length;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                labels.Add(new Label()); // intialisation
+                labels[i].Location = new Point((i * spaceBetween) + 10, 80); //The space between each underscore is always 40 on the same y-axis of 80. 
+                labels[i].Text = "___"; // Displays the underscore where the input will be displayed
+                labels[i].Parent = groupBox1; //Declaring where the above will be located
+                labels[i].BringToFront();
+                labels[i].CreateControl();
+            }
+            // The Word Length counter
+            lblWordLength.Text = "Word Length : " + (chars.Length).ToString() + " " + "letters";
+
+
+
+
+        }
+
+
+        string randoWord()
+        {
+
+            // Below here are the word the will be used in the game
+
+            string[] wordList = new string[10];
+
+            wordList[0] = "red";
+            wordList[1] = "blue";
+            wordList[2] = "black";
+            wordList[3] = "white";
+            wordList[4] = "orange";
+            wordList[5] = "gray";
+            wordList[6] = "coral";
+            wordList[7] = "teal";
+            wordList[8] = "tan";
+            wordList[9] = "violet";
+
+
+            //random class 
+
+            Random ran = new Random();
+            return wordList[ran.Next(0, wordList.Length)];
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LabelsChar();
+        }
+
+        public void resetHangman()
+        {
+            randoWord();
+            LabelsChar();
+            lblIncorrectGuesses.Text = "Missed: ";
+           // lblPlayerScore.Text = "The Score: ";
+
+
+
+        }
+
+        
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //The code below will allow the user to use keypresses when enter a letter
+            char guessedLetter = char.ToLower(e.KeyChar);
+            lblOutput.Text = guessedLetter.ToString();
+            
+
+            
+
+            //This code the computer to returns an error messsage when the user enters something thats not a letter
+            if (!char.IsLetter(guessedLetter))
+            {
+                MessageBox.Show("THIS TEXTBOX ONLY ACCEPTS LETTERS!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //The code that checks whether the guessed letter is found in chosen word
+            if (word.Contains(guessedLetter))
+            {
+                char[] guessedletters = word.ToCharArray();
+                for (int i = 0; i < guessedletters.Length; i++)
+                {
+                    if (guessedletters[i] == guessedLetter)
+                    {
+                        labels[i].Text = guessedLetter.ToString();
+
+                        // The score of the player is added
+                        score++;
+                        lblPlayerScore.Text = "Your current score: " + score.ToString();// Display the score to the label
+                        
+
+
+                        // The foreach loop checks the unscore place holderss if they are 
+                        foreach (Label b in labels)
+                            if (b.Text == "___") return;
+                        MessageBox.Show("Congratulations you have WON!", "WINNER!!!!!!");
+                        ;
+
+                        //After the user has guessed the word, the game to a new game 
+                        resetHangman();
+                        
+
+                        //This adds the score(s) of the the player with a timeStamp in Hours:Minutes:Seconds  
+                        DateTime timeStamp = DateTime.Now;
+                        string timeStampString = timeStamp.ToString("HH:mm:ss");
+                        string item1 = lblPlayerScore.Text;
+                        string formattedString = $"{timeStampString}:  {item1}";
+                        listBox1.Items.Add(formattedString);
+
+                    }
+
+
+
+
+
+                }
+            }
+            else
+            {
+                //The condition if you get the character entered incorrect 
+                MessageBox.Show("SORRY!! Wrong letter", "Try again!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);//Message for the incorrect word entered
+                lblIncorrectGuesses.Text += " " + guessedLetter.ToString() + ",";
+                tries++;
+
+
+                // if statement for when the number of guesses entred when playing [the maximum number of attempts is 10 for every word]
+                if (tries == 10)
+                {
+                    MessageBox.Show("Sorry man you have used all your attempts" + "\n" + "\n" + "The word was acctually: " + word + "\n" + "\n" + "Try again in the next game", "Game Over!!!!");
+                    resetHangman();
+
+                }
+
+            }
+
+           
+
+           
+        }
+
+
+        
+
+    
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            
+            resetHangman();
+            
+        }
+
+        private void lblPlayerScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void lblOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
